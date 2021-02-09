b702d35cef5aa0c2db0ddd9c957fdcc7def525c8
rrachinskiy <r.rostik2000@gmail.com>
2021-02-09 11:01:51 +0200

    Fix: Express-Session Type

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,11 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpError } from '../types';
 import User from '../models/User';
 
-export const auth = async (
-  req: Request & { session: { username: string } },
-  res: Response,
-  next: NextFunction,
-) => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const credentials = {
     username: req.body.username,
     password: req.body.password,
--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -3,7 +3,6 @@ import { auth } from '../../controllers/auth';
 
 const authRouter = Router();
 
-// @ts-ignore
 authRouter.post('/auth', auth);
 
 export default authRouter;
